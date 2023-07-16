e3dd3f48a0b3b9fb11317fb611e60a587901c3ff
James Bolton <james@james-bolton.com>
2023-07-16 23:34:50 +0100

    feat:  swap for vite site

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,8 +3,5 @@
 # create a dist folder if one doesnt exist already
 mkdir -p dist
 
-# Clear the dist folder
-rm -rf dist/*
-
 # build the tapes
 ./libs/zxbasic/zxbc.py ./games/helloworld.bas -o ./dist/helloworld.tap --tap --BASIC --autorun
