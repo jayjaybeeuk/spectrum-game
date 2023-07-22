1fbb2ec2d1c4d40a772a046ffc786545f48298de
James Bolton <james@james-bolton.com>
2023-07-22 09:50:27 +0100

    fix: build games first

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -16,7 +16,7 @@ const Home = () => {
     <>
       <Dropdown handleChange={handleOptionChange} value={selectedOption}>
         <option value="helloworld.tap">Hello World</option>
-        <option value="breakout.tap">Another Game</option>
+        <option value="breakout.tap">Breakout</option>
       </Dropdown>
       <div id="jsspeccy" ref={jssSpeccyRef}></div>
     </>
